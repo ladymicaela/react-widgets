e5e5b5814b062a55ee7378a1d1a367e4da9f7ded
Micaela Moss <mcmoss1991@gmail.com>
2020-03-06 14:56:43 -0800

    Add styling to tabs component

---
+++ frontend/tabs.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+class Headers extends React.Component {
+
+
+    render() {
+
+        let selected = this.props.selectedTab;
+
+        return (
+            <ul className="tab-header">
+                {this.props.tabs.map((tab, index) => {
+                    let title = tab.title;
+                    let klass = index == selected ? 'active' : '';
+                    return (
+                        <li
+                            key = {index}
+                            className = {klass}
+                            onClick = { () => this.props.onTabChosen(index)}
+                        >
+                            {title}
+                        </li>
+                    )
+                })}
+            </ul>
+        )
+    }
+}
+
+
+class Tabs extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedTab: 0
+        }
+        this.selectTab = this.selectTab.bind(this);
+    }
+
+    selectTab(tabIdx) {
+        this.setState({selectedTab: tabIdx });
+    }
+
+    render() {
+
+        let tab = this.props.tabs[this.state.selectedTab];
+
+        return (
+            <div className="tabs">
+                <h1 className="tabs-header">React Tabs</h1>
+                <div>
+                    <Headers 
+                        selectedTab = {this.state.selectedTab}
+                        tabs = {this.props.tabs}
+                        onTabChosen = {this.selectTab}
+                    />
+                    <div className="tab-content">
+                        <article>
+                            {tab.content}
+                        </article>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+};
+
+export default Tabs;
--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 import Clock from './clock';
+import Tabs from './tabs';
+
+const tabs = [
+    {title: 'one' , content: 'I am the first tab'},
+    {title: 'two', content: 'I am the second tab'},
+    {title: 'three', content: 'I am the third tab'}
+]
+
+
+
+
+
 
 const Widgets = () => (
     <div>
         <Clock />
+        <Tabs tabs={tabs}/>
     </div>
 );
 
