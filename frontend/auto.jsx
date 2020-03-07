59a2dd21e90111be4e81775af861bf74494def90
Micaela Moss <mcmoss1991@gmail.com>
2020-03-06 16:16:22 -0800

    Create Auto component

---
+++ frontend/auto.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class Auto extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        
+        return(
+            <div>
+                I am the auto complete component
+            </div>
+        )
+    }
+
+};
+
+export default Auto;
--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Clock from './clock';
 import Tabs from './tabs';
+import Auto from './auto';
 
 const tabs = [
     {title: 'one' , content: 'I am the first tab'},
@@ -9,15 +10,24 @@ const tabs = [
     {title: 'three', content: 'I am the third tab'}
 ]
 
-
-
-
-
+const names = [
+    'Anthony',
+    'Eddie',
+    'Jared',
+    'Jason',
+    'JP',
+    'Margaret',
+    'Micaela',
+    'Mike',
+    'Shawn',
+    'Touba' 
+]
 
 const Widgets = () => (
     <div>
         <Clock />
         <Tabs tabs={tabs}/>
+        <Auto names={names}/>
     </div>
 );
 
