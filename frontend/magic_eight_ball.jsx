b0d68ffc7ba47bc13446fa8bd8f0d133c884a3fb
Micaela Moss <mcmoss1991@gmail.com>
2020-03-24 09:44:20 -0700

    Create askQuestion function

--- frontend/magic_eight_ball.jsx
+++ frontend/magic_eight_ball.jsx
@@ -5,12 +5,19 @@ class MagicEightBall extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            responses = this.props.responses,
             response = ""
         }
     }
 
 
+    askQuestion() {
+        const responses = this.props.responses;
+        let randomResponse = responses[Math.floor(Math.random() * responses.length)];
+        this.setState({
+            response: randomResponse
+        })
+
+    }
 
 
     render() {
