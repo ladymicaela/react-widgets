7f1c1aef756a48fa7f3e21f0d3a5de4b61926217
Micaela Moss <mcmoss1991@gmail.com>
2020-03-31 15:19:20 -0700

    Create dice component

---
+++ frontend/dice.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Dice extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        return (
+            <div>I am the dice component</div>
+        )
+    }
+
+
+};
+
+export default Dice;
